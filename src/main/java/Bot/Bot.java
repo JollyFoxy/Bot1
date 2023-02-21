39c3326dc958d75b2b7f2f39549f1b84e5550385
pavel <pavel.mizirev@gmail.com>
2023-02-21 17:17:41 +0300

    Да начнуться кривляния

---
+++ src/main/java/Bot/Bot.java
@@ -0,0 +1,6 @@
+import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
+
+public final class Bot extends TelegramLongPollingCommandBot {
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+}
